ae70a58cfb99cabca4f8277a3585c92b58e554ee
Francisco López <fran.jlp26@gmail.com>
2022-09-13 23:55:12 -0600

    Add gray-matter logic

---
+++ lib/markdown.ts
@@ -0,0 +1,38 @@
+import fs from 'fs'
+import matter from 'gray-matter'
+import path from 'path'
+
+const root = process.cwd()
+
+interface PropFileBySlug {
+  type: string
+  slug: string
+}
+
+export const getFiles = async (type: string) => {
+  return fs.readdirSync(path.join(root, 'data', type))
+}
+
+export const getAllFilesFrontMatter = async (type: string) => {
+  const files = await getFiles(type)
+
+  const frontMatterFiles = files.map((file) => {
+    const source = fs.readFileSync(path.join(root, 'data', type, file), 'utf-8')
+    console.log(source)
+    const { data } = matter(source)
+    return { ...data, slug: `${type}/${file.replace('.md', '')}` }
+  })
+
+  return frontMatterFiles
+}
+
+export const getfilesBySlug = async ({ type, slug }: PropFileBySlug) => {
+  const mdSource = fs.readFileSync(
+    path.join(root, 'data', type, `${slug}.md`),
+    'utf-8'
+  )
+
+  const { data: frontMatter, content } = matter(mdSource)
+
+  return { frontMatter, content }
+}
