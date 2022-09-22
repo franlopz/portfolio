a8a77f24d10b778a4222db299080d8e9b698c8e3
Francisco López <fran.jlp26@gmail.com>
2022-09-22 01:49:56 -0600

    Add error handling in markdown functions

--- lib/markdown.ts
+++ lib/markdown.ts
@@ -10,28 +10,37 @@ interface PropFileBySlug {
 }
 
 export const getFiles = async (type: string) => {
-  return fs.readdirSync(path.join(root, 'data', type))
+  try {
+    return fs.readdirSync(path.join(root, 'data', type))
+  } catch (e) {}
 }
 
 export const getAllFilesFrontMatter = async (type: string) => {
   const files = await getFiles(type)
 
-  const frontMatterFiles = files.map((file) => {
-    const source = fs.readFileSync(path.join(root, 'data', type, file), 'utf-8')
-    const { data } = matter(source)
-    return { ...data, slug: `${type}/${file.replace('.mdx', '')}` }
-  })
+  try {
+    const frontMatterFiles = files?.map((file) => {
+      const source = fs.readFileSync(
+        path.join(root, 'data', type, file),
+        'utf-8'
+      )
+      const { data } = matter(source)
+      return { ...data, slug: `${type}/${file.replace('.mdx', '')}` }
+    })
 
-  return frontMatterFiles
+    return frontMatterFiles
+  } catch (e) {}
 }
 
 export const getfilesBySlug = async ({ type, slug }: PropFileBySlug) => {
-  const mdSource = fs.readFileSync(
-    path.join(root, 'data', type, `${slug}.mdx`),
-    'utf-8'
-  )
+  try {
+    const mdSource = fs.readFileSync(
+      path.join(root, 'data', type, `${slug}.mdx`),
+      'utf-8'
+    )
 
-  const { data: frontMatter, content } = matter(mdSource)
+    const { data: frontMatter, content } = matter(mdSource)
 
-  return { frontMatter, content }
+    return { frontMatter, content }
+  } catch (e) {}
 }
